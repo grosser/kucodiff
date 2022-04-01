f953be8e279370109c2521935f8bd6713056d57b
Michael Grosser <michael@grosser.it>
2022-03-31 21:38:41 -0700

    everything is already in temp

--- spec/kucodiff_spec.rb
+++ spec/kucodiff_spec.rb
@@ -65,94 +65,76 @@ describe Kucodiff do
     end
 
     it "shows where parse errors are" do
-      in_temp_dir do
-        File.write("a.yml", {"foo" => 1}.to_yaml)
-        File.write("b.yml", "[mysqld]\nstuff")
-        expect { Kucodiff.diff(['a.yml', 'b.yml']) }.to raise_error(/\(b.yml\): did not find/)
-      end
+      File.write("a.yml", {"foo" => 1}.to_yaml)
+      File.write("b.yml", "[mysqld]\nstuff")
+      expect { Kucodiff.diff(['a.yml', 'b.yml']) }.to raise_error(/\(b.yml\): did not find/)
     end
 
     it "reads the first object when multiple exist" do
-      in_temp_dir do
-        File.write("a.yml", {"foo" => 1}.to_yaml + {"bar" => 1}.to_yaml)
-        File.write("b.yml", {"foo" => 2}.to_yaml)
-        expect(Kucodiff.diff(['a.yml', 'b.yml'])).to eq({"a.yml-b.yml" => ["foo"]})
-      end
+      File.write("a.yml", {"foo" => 1}.to_yaml + {"bar" => 1}.to_yaml)
+      File.write("b.yml", {"foo" => 2}.to_yaml)
+      expect(Kucodiff.diff(['a.yml', 'b.yml'])).to eq({"a.yml-b.yml" => ["foo"]})
     end
 
     it "can ignore" do
-      in_temp_dir do
-        File.write("a.yml", {"spec" => {"template" => {"spec" => {"containers" => [{"command" => ["a", "b"]}]}}}}.to_yaml)
-        File.write("b.yml", {"spec" => {"template" => {"spec" => {"containers" => [{"command" => ["c", "d"]}]}}}}.to_yaml)
-        expect(Kucodiff.diff(['a.yml', 'b.yml'], ignore: /\.command\./)).to eq({"a.yml-b.yml" => []})
-      end
+      File.write("a.yml", {"spec" => {"template" => {"spec" => {"containers" => [{"command" => ["a", "b"]}]}}}}.to_yaml)
+      File.write("b.yml", {"spec" => {"template" => {"spec" => {"containers" => [{"command" => ["c", "d"]}]}}}}.to_yaml)
+      expect(Kucodiff.diff(['a.yml', 'b.yml'], ignore: /\.command\./)).to eq({"a.yml-b.yml" => []})
     end
 
     it "removes things that are expected" do
-      in_temp_dir do
-        File.write("a.yml", {"a" => 1}.to_yaml)
-        File.write("b.yml", {"a" => 1, "b" => 2}.to_yaml)
-        expect(Kucodiff.diff(['a.yml', 'b.yml'], expected: {"a.yml-b.yml" => ["b"]})).to eq({})
-      end
+      File.write("a.yml", {"a" => 1}.to_yaml)
+      File.write("b.yml", {"a" => 1, "b" => 2}.to_yaml)
+      expect(Kucodiff.diff(['a.yml', 'b.yml'], expected: {"a.yml-b.yml" => ["b"]})).to eq({})
     end
 
     it "adds things that are unexpected" do
-      in_temp_dir do
-        File.write("a.yml", {"a" => 1}.to_yaml)
-        File.write("b.yml", {"a" => 1, "b" => 2}.to_yaml)
-        expect(Kucodiff.diff(['a.yml', 'b.yml'], expected: {"a.yml-b.yml" => ["c"], "a.yml-z.yml" => ["a"]})).to eq(
-          "a.yml-b.yml" => ["b", "c"], "a.yml-z.yml" => ["a"]
-        )
-      end
+      File.write("a.yml", {"a" => 1}.to_yaml)
+      File.write("b.yml", {"a" => 1, "b" => 2}.to_yaml)
+      expect(Kucodiff.diff(['a.yml', 'b.yml'], expected: {"a.yml-b.yml" => ["c"], "a.yml-z.yml" => ["a"]})).to eq(
+        "a.yml-b.yml" => ["b", "c"], "a.yml-z.yml" => ["a"]
+      )
     end
 
     it "converts required_env to an array" do
-      in_temp_dir do
-        File.write("a.yml", {"spec" => {"template" => {"metadata" => {"annotations" => {"samson/required_env" => "  a\nb,c d  "}}}}}.to_yaml)
-        File.write("b.yml", {"spec" => {"template" => {"metadata" => {"annotations" => {"samson/required_env" => "f\nb,e d"}}}}}.to_yaml)
-        expect(Kucodiff.diff(['a.yml', 'b.yml'])).to eq("a.yml-b.yml" => [
-          "spec.template.metadata.annotations.samson/required_env.a",
-          "spec.template.metadata.annotations.samson/required_env.c",
-          "spec.template.metadata.annotations.samson/required_env.e",
-          "spec.template.metadata.annotations.samson/required_env.f",
-        ])
-      end
+      File.write("a.yml", {"spec" => {"template" => {"metadata" => {"annotations" => {"samson/required_env" => "  a\nb,c d  "}}}}}.to_yaml)
+      File.write("b.yml", {"spec" => {"template" => {"metadata" => {"annotations" => {"samson/required_env" => "f\nb,e d"}}}}}.to_yaml)
+      expect(Kucodiff.diff(['a.yml', 'b.yml'])).to eq("a.yml-b.yml" => [
+        "spec.template.metadata.annotations.samson/required_env.a",
+        "spec.template.metadata.annotations.samson/required_env.c",
+        "spec.template.metadata.annotations.samson/required_env.e",
+        "spec.template.metadata.annotations.samson/required_env.f",
+      ])
     end
 
     describe "with indent_pod" do
       it "produces full diff when comparing non-pods" do
-        in_temp_dir do
-          template = {"metadata" => {"annotations" => {"foo" => "bar"}}}
-          File.write("a.yml", {"spec" => {"template" => template, "other" => "ignores"}}.to_yaml)
-          File.write("b.yml", template.to_yaml)
-          expect(Kucodiff.diff(['a.yml', 'b.yml'], indent_pod: true)).to eq("a.yml-b.yml" => [
-            "metadata.annotations.foo",
-            "spec.other",
-            "spec.template.metadata.annotations.foo"
-          ])
-        end
+        template = {"metadata" => {"annotations" => {"foo" => "bar"}}}
+        File.write("a.yml", {"spec" => {"template" => template, "other" => "ignores"}}.to_yaml)
+        File.write("b.yml", template.to_yaml)
+        expect(Kucodiff.diff(['a.yml', 'b.yml'], indent_pod: true)).to eq("a.yml-b.yml" => [
+          "metadata.annotations.foo",
+          "spec.other",
+          "spec.template.metadata.annotations.foo"
+        ])
       end
 
       it "produces little diff when comparing a pod with a deployment" do
-        in_temp_dir do
-          template = {"metadata" => {"annotations" => {"foo" => "bar"}}}
-          File.write("a.yml", {"spec" => {"template" => template, "other" => "ignores"}}.to_yaml)
-          File.write("b.yml", template.merge("kind" => "Pod").to_yaml)
-          expect(Kucodiff.diff(['a.yml', 'b.yml'], indent_pod: true)).to eq("a.yml-b.yml" => [
-            "spec.template.kind",
-          ])
-        end
+        template = {"metadata" => {"annotations" => {"foo" => "bar"}}}
+        File.write("a.yml", {"spec" => {"template" => template, "other" => "ignores"}}.to_yaml)
+        File.write("b.yml", template.merge("kind" => "Pod").to_yaml)
+        expect(Kucodiff.diff(['a.yml', 'b.yml'], indent_pod: true)).to eq("a.yml-b.yml" => [
+          "spec.template.kind",
+        ])
       end
 
       it "creates full diff when comparing a pod with a pod" do
-        in_temp_dir do
-          template = {"metadata" => {"annotations" => {"foo" => "bar"}}, "kind" => "Pod"}
-          File.write("a.yml", template.to_yaml)
-          File.write("b.yml", template.merge("foo" => "bar").to_yaml)
-          expect(Kucodiff.diff(['a.yml', 'b.yml'], indent_pod: true)).to eq("a.yml-b.yml" => [
-            "spec.template.foo",
-          ])
-        end
+        template = {"metadata" => {"annotations" => {"foo" => "bar"}}, "kind" => "Pod"}
+        File.write("a.yml", template.to_yaml)
+        File.write("b.yml", template.merge("foo" => "bar").to_yaml)
+        expect(Kucodiff.diff(['a.yml', 'b.yml'], indent_pod: true)).to eq("a.yml-b.yml" => [
+          "spec.template.foo",
+        ])
       end
     end
   end
